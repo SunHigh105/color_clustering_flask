7d308499b6bd43953afaa77d161fafeb55acfee1
Ayaka Hidaka <ayakahidaka@AyakanoMacBook-Pro.local>
2020-07-24 16:24:41 +0900

    delete navigation from header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,6 @@
   <div id="app">
     <header>
       <h2>Image Color Clustering</h2>
-      <nav>
-        <ul>
-          <router-link tag="li" to="/upload"><a>Upload</a></router-link>
-          <router-link tag="li" to="/list"><a>File List</a></router-link>
-        </ul>
-      </nav>
     </header>
     <router-view/>
   </div>
